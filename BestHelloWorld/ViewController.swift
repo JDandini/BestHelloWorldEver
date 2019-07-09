e28a5a12a27b02d5c1f986829bcf67427e3a32fb
Javier Castañeda <javier.castaneda@gaiadesign.com.mx>
2019-07-09 17:15:54 -0500

    Initial Commit

---
+++ BestHelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestHelloWorld
+//
+//  Created by F J Castaneda Ramos on 7/9/19.
+//  Copyright © 2019 F J Castaneda Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
